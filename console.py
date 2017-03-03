408321a52847fa5c4ea1dd75823192cbf222fae0
duckrxy <duckrxy@hotmail.com>
2017-03-04 01:11:34 +0800

    add actress retrieve function

--- console.py
+++ console.py
@@ -7,15 +7,18 @@ import os
 # print(os.path.abspath(os.path.curdir))
 os.path.join(os.path.abspath(os.path.curdir), 'test')
 jav = javlibrary.JavLibrary()
-#print(jav.SearchNumber('KV-164'))
+#print(jav.SearchNumber('adn-011'))
+info = jav.SearchNumber('adn-011')
+print(info['actress'])
+print(info['title'])
 #os.mkdir(os.path.join(os.path.abspath(os.path.curdir), 'test'))
 #os.rmdir(os.path.join(os.path.abspath(os.path.curdir), 'test'))
-result = os.scandir('\\\\TIGER-NAS\\Ad\\日本专业\\浜崎真緒\\')
-for item in result:
-    #print(item.name, item.path)
-    print(item.name)
-    print(re.findall('^([a-zA-Z]{2,5}-[0-9]{2,5}).*', item.name))
-    print(jav.SearchNumber(re.findall('^([a-zA-Z]{2,5}-[0-9]{2,4}).*', item.name)[0]))
-    time.sleep(1)
+# result = os.scandir('\\\\TIGER-NAS\\Ad\\日本专业\\浜崎真緒\\')
+# for item in result:
+#     #print(item.name, item.path)
+#     print(item.name)
+#     print(re.findall('^([a-zA-Z]{2,5}-[0-9]{2,5}).*', item.name))
+#     print(jav.SearchNumber(re.findall('^([a-zA-Z]{2,5}-[0-9]{2,4}).*', item.name)[0]))
+#     time.sleep(1)
 
     
--- javlibrary.py
+++ javlibrary.py
@@ -2,21 +2,22 @@
 import re
 import requests
 
+
 class JavLibrary(object):
     __header__ = {
-    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-    "Accept-Encoding":"gzip, deflate, sdch",
-    "Accept-Language":"en-US,en;q=0.8",
-    "Connection":"keep-alive",
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-    "Referer":"http://www.javlibrary.com/cn/",
-}
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, sdch",
+        "Accept-Language": "en-US,en;q=0.8",
+        "Connection": "keep-alive",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+        "Referer": "http://www.javlibrary.com/cn/",
+    }
     __javHomeUrl__ = 'http://www.javlibrary.com/cn/'
     __javSearchUrl__ = 'http://www.javlibrary.com/cn/vl_searchbyid.php'
 
     def __init__(self):
         self.session = requests.Session()
-        self.session.get(self.__javHomeUrl__, headers = self.__header__)
+        self.session.get(self.__javHomeUrl__, headers=self.__header__)
 
     def GetTitle(self, text, reg):
         titles = re.findall(reg, text)
@@ -24,22 +25,33 @@ class JavLibrary(object):
             return titles[0]
         else:
             return ''
-    
+
+    def GetActress(self, text):
+        reg = '<a href=\"vl_star.php\?s=.{2,6}\" rel=\"tag\">(.*)<\/a>'
+        actresses = re.findall(reg, text)
+        return actresses[0]
+
     def SearchNumber(self, number):
         upperNumber = str(number).upper()
-        query = {'keyword':upperNumber}
-        r = self.session.get(self.__javSearchUrl__, headers=self.__header__,params = query)
+        query = {'keyword': upperNumber}
+        r = self.session.get(self.__javSearchUrl__,
+                             headers=self.__header__, params=query)
         title = self.GetTitle(r.text, '<title>(.*) - JAVLibrary<\/title>')
         if (title == ""):
             raise Exception('Not Found')
         if (title.find('识别码搜寻结果') != -1):
-            redirectedUrlReg = str.format("<a href=\"(.*)\" title=\"{0}.*\">",upperNumber)
-            redirectedPageUrl = self.__javHomeUrl__ + self.GetTitle(r.text, redirectedUrlReg)
-            redirectedPage = self.session.get(redirectedPageUrl, headers = self.__header__)
-            redirectedTitle = self.GetTitle(redirectedPage.text, '<title>(.*) - JAVLibrary<\/title>')
+            redirectedUrlReg = str.format(
+                "<a href=\"(.*)\" title=\"{0}.*\">", upperNumber)
+            redirectedPageUrl = self.__javHomeUrl__ + \
+                self.GetTitle(r.text, redirectedUrlReg)
+            redirectedPage = self.session.get(
+                redirectedPageUrl, headers=self.__header__)
+            redirectedTitle = self.GetTitle(
+                redirectedPage.text, '<title>(.*) - JAVLibrary<\/title>')
             if (redirectedTitle != ""):
-                return redirectedTitle
+                actress = self.GetActress(redirectedPage.text)
+                return {'title': redirectedTitle, 'actress': actress}
             else:
                 raise Exception('Not Found')
-        return title
-            
+        actress = self.GetActress(r.text)
+        return {'title': title, 'actress': actress}
